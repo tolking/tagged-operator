2f80b738ac33d53d7abcb0fce0388b8b32bb15cb
wang qiang <qw13131wang@gmail.com>
2022-04-21 17:03:14 +0800

    refacto: split code

--- src/core/class.ts
+++ src/core/class.ts
@@ -1,26 +1,11 @@
-import type { TemplateOperatorConfig, ValueType } from '../types/index'
+import { createCalc } from './index'
+import type { TemplateOperatorConfig } from '../types/index'
 
 export class TemplateOperator {
-  private config
+  public calc
 
   constructor(config: TemplateOperatorConfig) {
-    this.config = config
+    this.calc = createCalc(config)
+    // TODO: add more operations, eg: compare? ...
   }
-
-  calc(strings: TemplateStringsArray, ...arg: ValueType[]) {
-    const len = strings.length
-
-    // TODO: Operator Weight Judgment
-    return strings.reduce((all, item, index) => {
-      const _item = item.trim()
-      if (_item) {
-        return this.config.operator(_item, all, arg[index])
-      } else if (index !== 0 && index !== len - 1) {
-        console.warn('miss operator')
-      }
-      return all
-    }, arg[0])
-  }
-
-  // TODO: add more operations, eg: compare? ...
 }
